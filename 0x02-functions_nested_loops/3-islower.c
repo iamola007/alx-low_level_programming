8a2809d2a2e459610921f1e9b516d6d2d292fe20
iamola007 <olaoladapo007@gmail.com>
2022-04-13 05:12:32 -0700

    add 3-islower.c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check for lower case letter
+ * @c : character to check th case
+ * Return:0 or 1
+*/
+
+int _islower(int c)
+{
+	return (s >= 97 && c < 122);
+}
