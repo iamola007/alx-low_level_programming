b52c878c15ebad1ef6e12fe46b53f8afad5608d1
iamola007 <olaoladapo007@gmail.com>
2022-04-20 15:34:10 -0700

    my first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates an integer value to 98
+ * @n: a pointer to an int to be updated
+ * Return: 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+How to use pointers, arrays and strings in c
