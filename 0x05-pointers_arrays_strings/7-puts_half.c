5b85a329cb28aa1f0e847e8c5731eeb7b89b3144
iamola007 <olaoladapo007@gmail.com>
2022-04-20 16:49:40 -0700

    add 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to an int that will change
+ * Return: 0
+*/
+
+void puts_half(char *str)
+{
+	int i, last;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	last = (i + 1) / 2;
+
+	for (i = last; str[i]; i++)
+	{
+		_putchar (str[i]);
+	}
+	_putchar ('\n');
+}
