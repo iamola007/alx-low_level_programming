1cdf4696644778c963d2a510d5fc2792b9e7d731
iamola007 <olaoladapo007@gmail.com>
2022-04-20 16:39:50 -0700

    add 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every char of a string
+ * @str: a pointer to be changed
+ * Return: 0
+*/
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+		{
+			_putchar (str[a]);
+		}
+	}
+	_putchar ('\n');
+}
