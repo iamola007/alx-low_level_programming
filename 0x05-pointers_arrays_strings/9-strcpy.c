068289364d345eb81703cb79e235582787e305e4
iamola007 <olaoladapo007@gmail.com>
2022-04-20 17:10:24 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: a pointer to the string that will be copied to
+ * @src: a string that will be copied
+ * Return: dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
