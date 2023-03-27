447c28081f601c3dc5726c1f43b39358b9c87c74
iamola007 <olaoladapo007@gmail>
2023-03-27 04:03:10 -0700

    add a main.h file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,31 +1,24 @@
 #include "main.h"
 
 /**
- *  * _strcat - concatenates two strings
- *   * @dest: input parameter string
- *    * @src: input parameter string
- *     *
- *      * Return: dest
-*/
+ * _strcat - Concatenates the string pointed to by @src,
+ * null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
+ *
+ */
+
 char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
+	int index = 0, dest_len = 0;
 
-	a = 0;
+	while (dest[index++])
+		dest_len++;
 
-	while (dest[a] != 0)
-	{
-		a++;
-	}
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-	b = 0;
-
-	while (src[b] != 0)
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
 	return (dest);
 }
